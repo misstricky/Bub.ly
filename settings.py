ce0d32f1110f5aa428e3dea55813a5492184edcc
Yashh <yash888@gmail.com>
2010-08-14 08:30:50 -0700

    moved the apps into apps folder and adjusted settings to put apps on path

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 # Django settings for bubly project.
-import os
+import os, sys
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
