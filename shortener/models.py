d745517b32a22d5450a1ccf2557c46f1170923aa
Yashh <yash888@gmail.com>
2010-08-14 01:39:01 -0700

    writing a django like model to mimic some ORM like functionality

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+# nope we're using redis :)
+# mimic a django like model with some methods to fetch data
 
-# Create your models here.
+class UrlModel(object):
+    key_prefix = "url:"
+    key_counter = "counter:url"
+
+    def __init__(self, url_data={}):
+        self.url = url_data.get('url', None)
+        self.hits = url_data.get('hits', 0)
+        self.created_at = url_data.get('created_at', time.time()+7*60*60)
+        self.is_file = url_data.get('is_file', False)
+
+
