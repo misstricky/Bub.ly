cd1a4545f6ec9f2b24d4527323d3654e510fa4d6
Yashh <yash888@gmail.com>
2010-08-14 01:40:25 -0700

    adding a DoesNotExist exception to raise when get does not return object

--- shortener/models.py
+++ shortener/models.py
@@ -1,6 +1,9 @@
 # nope we're using redis :)
 # mimic a django like model with some methods to fetch data
 
+class DoesNotExist(Exception):
+    pass
+
 class UrlModel(object):
     key_prefix = "url:"
     key_counter = "counter:url"
@@ -11,4 +14,9 @@ class UrlModel(object):
         self.created_at = url_data.get('created_at', time.time()+7*60*60)
         self.is_file = url_data.get('is_file', False)
 
-
+    @classmethod
+    def get_by_id(self, url_id):
+        url_data = redis_ob.hgetall(url_id)
+        if not url_data: raise DoesNotExist
+        url_data_id = int(url_id.lstrip("url:"))
+        return self(url_data=url_data, url_id=url_data_id)
