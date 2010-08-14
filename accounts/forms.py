5755c5e7892f120f4e8fd8a9318dd9307735b8ea
Yashh <yash888@gmail.com>
2010-08-14 00:30:13 -0700

    saving user data into redis using pipeline to mimic a transaction

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,6 @@
+import random
 from django import forms
+from django.utils.hashcompat import md5_constructor, sha_constructor
 from connect_redis import get_client
 redis_ob = get_client()
 
@@ -13,4 +15,15 @@ class RegisterForm(forms.Form):
         return self.cleaned_data['email']
 
     def save(self):
-        pass
+        # get the user id by incrementing counter
+        user_id = redis_ob.incr("counter:user")
+        # generating salt. Trying to mimic django auth app password generation
+        salt = sha_constructor(str(random.random()) +str(random.random())).hexdigest()[:5]
+        hsh = sha_constructor(str(random.random()) +self.cleaned_data["password"]).hexdigest()
+        # using redis pipeline to make it happen as a transaction.
+        redis_pipe = redis_ob.pipeline()
+        redis_pipe.set("user:email:%s" %md5_constructor(self.cleaned_data['email']).hexdigest(), user_id).hmset("user:%d" %user_id, {"email": self.cleaned_data["email"], "password": "%s$%s" %(salt, hsh)})
+        redis_pipe.execute()
+        return user_id
+
+
--- docs/data_modelling.md
+++ docs/data_modelling.md
@@ -1,5 +1,6 @@
 key                           -                   data
 ==================================================================
+counter:user                  -                   1
 user:email:<md5(email)>       -                 <user_id>  eg: 1
 user:<user_id>                -       {"email": "some@some.com",
                                        "password": "s3fdd$jlh32hwk32k23k"}
