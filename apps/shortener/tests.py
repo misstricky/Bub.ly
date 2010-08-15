cac90d0cf78169d14a8341397725ec25c0ad8c1a
Yashh <yash888@gmail.com>
2010-08-15 10:11:27 -0700

    adding a tweet this button to facebox. adding a about page

--- apps/shortener/tests.py
+++ apps/shortener/tests.py
@@ -15,15 +15,15 @@ class FunctionalityTest(TestCase):
     def test_register(self):
         response = self.client.get('/')
         self.failUnlessEqual(response.status_code, 200)
-        response = self.client.post('/_register/', {'email': 'yash888@gmail.com', 'password': '123456'}, follow=True)
+        response = self.client.post('/_register/', {'email': 'some@something.com', 'password': '123456'}, follow=True)
         self.assertEquals(response.status_code, 200)
         self.assertEquals(response.redirect_chain, [('http://testserver/', 302)])
-        self.assertEquals(True, redis_ob.exists("user:email:%s" %md5_constructor('yash888@gmail.com').hexdigest()))
+        self.assertEquals(True, redis_ob.exists("user:email:%s" %md5_constructor('some@something.com').hexdigest()))
         # cant assert the user:1 because redis might already have a user:1
         # and we dont clear redis data 
 
     def test_login(self):
-        response = self.client.post('/_login/', {'email': 'yash888@gmail.com', 'password': '123456'}, follow=True)
+        response = self.client.post('/_login/', {'email': 'some@something.com', 'password': '123456'}, follow=True)
         self.assertEquals(response.status_code, 200)
         # need to assure some text from logged in home
         self.assertContains(response, "")
